83363d4c1822b8913c2353dbe4040593d6b550b2
Lucas Costa <lucascosta74@gmail.com>
2020-03-21 16:06:29 -0300

    definicao de estilo

---
+++ lib/controle/estilo.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:pesquisadorhinos/modelador.dart';
+
+final TextTheme fontesPadrao = TextTheme(
+  headline: TextStyle(fontFamily: 'Oxanium'),
+  title: TextStyle(fontFamily: 'Oxanium'),
+  subtitle: TextStyle(fontFamily: 'Raleway'),
+  button: TextStyle(fontFamily: 'Raleway'),
+);
+
+final ThemeData estiloPadrao = ThemeData(
+    // padrao de cores
+    brightness: Brightness.light,
+    primaryColor: CoresPadrao.azul[30],
+    primaryColorBrightness: Brightness.light,
+    accentColor: CoresPadrao.azul[20],
+    accentColorBrightness: Brightness.light,
+    backgroundColor: Colors.white,
+    // fonte padrao
+    fontFamily: 'Roboto',
+    textTheme: fontesPadrao,
+    // outros
+    splashColor: CoresPadrao.azul[30],
+    buttonColor: CoresPadrao.azul[20],
+    buttonTheme: ButtonThemeData(textTheme: ButtonTextTheme.accent));
