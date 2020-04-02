cd6bec60478c2cbc1d85e5e722a6bc2cd305eb0e
Lucas Costa <lucascosta74@gmail.com>
2020-04-01 22:05:05 -0300

    widgets de telas prontas

--- lib/vista/pesquisador.dart
+++ lib/vista/pesquisador.dart
@@ -19,135 +19,73 @@ class _PesquisadorAppState extends State<PesquisadorApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 20),
-                  height: 36,
-                  child: Image(
-                    image: AssetImage('assets/titulo.png'),
-                  )),
-            ),
-            Container(
-              width: 320,
-              child: TextField(
-                cursorColor: RequisitaCor.requisitaCinza(40),
-                style: TextStyle(fontSize: 18),
-                decoration: InputDecoration(
-                    suffixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide(
-                          width: 0.0,
-                        )),
-                    contentPadding: EdgeInsets.symmetric(horizontal: 24)),
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemBuilder: (context, position) {
-                  return Container(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-                    child: Flex(
-                      direction: Axis.horizontal,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                'titulo',
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontFamily: 'Raleway',
-                                ),
-                              ),
-                              Text(
-                                'categoria',
-                                style: TextStyle(
-                                  color: RequisitaCor.requisitaCinza(30),
-                                  fontFamily: 'Raleway',
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 16.0),
-                                child: Text('parte do hino'),
-                              )
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: <Widget>[
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.end,
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: <Widget>[
-                                  Text(
-                                    'nº',
-                                    style: TextStyle(
-                                        fontFamily: 'Raleway',
-                                        color: RequisitaCor.requisitaAzul(20)),
-                                  ),
-                                  Text(
-                                    'XX',
-                                    style: TextStyle(
-                                        fontFamily: 'Raleway',
-                                        color: RequisitaCor.requisitaAzul(30),
-                                        fontSize: 20),
-                                  ),
-                                ],
-                              ),
-                              ClipRRect(
-                                borderRadius: BorderRadius.only(
-                                  bottomLeft: Radius.circular(20),
-                                  topLeft: Radius.circular(20),
-                                ),
-                                child: Container(
-                                    color: RequisitaCor.requisitaAzul(30),
-                                    padding: EdgeInsets.symmetric(vertical: 3, horizontal: 6),
-                                    child: Text(
-                                      'Coletanea',
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 12),
-                                    )),
-                              )
-                            ],
-                          ),
-                        ),
-                        Container(
-                          height: 10,
-                        )
-                      ],
-                    ),
-                  );
-                },
-                itemCount: 2,
-              ),
-            )
-          ],
-        ),
-      ),
+          child: //Inicial()
+          DurantePesquisa(),
+          ),
 
       /* 
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
+        body:  */
+    );
+  }
+}
+
+class Inicial extends StatelessWidget {
+  const Inicial({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+    Center(
+      child: Container(
+          width: 240,
+          child: Image(
+            image: AssetImage('assets/logo.png'),
+          )),
+    ),
+    SizedBox(
+      height: 40,
+    ),
+    Container(
+      width: 320,
+      child: TextField(
+        cursorColor: RequisitaCor.requisitaCinza(40),
+        style: TextStyle(fontSize: 18),
+        decoration: InputDecoration(
+            suffixIcon: Icon(Icons.search),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                borderSide: BorderSide(
+                  width: 0.0,
+                )),
+            contentPadding: EdgeInsets.symmetric(horizontal: 24)),
+      ),
+    ),
+        ],
+      );
+  }
+}
+
+class DurantePesquisa extends StatelessWidget {
+  const DurantePesquisa({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
         Center(
           child: Container(
-              width: 240,
+              margin: EdgeInsets.symmetric(vertical: 20),
+              height: 36,
               child: Image(
-                image: AssetImage('assets/logo.png'),
+                image: AssetImage('assets/titulo.png'),
               )),
-        ),
-        SizedBox(
-          height: 40,
         ),
         Container(
           width: 320,
@@ -164,8 +102,173 @@ class _PesquisadorAppState extends State<PesquisadorApp> {
                 contentPadding: EdgeInsets.symmetric(horizontal: 24)),
           ),
         ),
+        //PrePesquisa()
+        //ListagemDeHinos()
+        //SemResultados()
       ],
-    ) */
+    );
+  }
+}
+
+class SemResultados extends StatelessWidget {
+  const SemResultados({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.only(bottom: 22),
+              width: 180,
+              child: Image(
+                image: AssetImage('assets/semAchar.png'),
+              )),
+          Text(
+            'Nenhum hino encontrado',
+            style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 24,
+                color: RequisitaCor.requisitaCinza(30)),
+          ),
+          SizedBox(
+            height: 50,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PrePesquisa extends StatelessWidget {
+  const PrePesquisa({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.only(bottom: 22),
+              width: 180,
+              child: Image(
+                image: AssetImage('assets/prePesquisa.png'),
+              )),
+          Text(
+            'Comece a pesquisar...',
+            style: TextStyle(
+                fontFamily: 'Raleway',
+                fontSize: 24,
+                color: RequisitaCor.requisitaCinza(30)),
+          ),
+          SizedBox(
+            height: 50,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ListagemDeHinos extends StatelessWidget {
+  const ListagemDeHinos({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemBuilder: (context, position) {
+          return Container(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+            child: Flex(
+              direction: Axis.horizontal,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        'titulo',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontFamily: 'Raleway',
+                        ),
+                      ),
+                      Text(
+                        'categoria',
+                        style: TextStyle(
+                          color: RequisitaCor.requisitaCinza(30),
+                          fontFamily: 'Raleway',
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0),
+                        child: Text('parte do hino'),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'nº',
+                            style: TextStyle(
+                                fontFamily: 'Raleway',
+                                color: RequisitaCor.requisitaAzul(20)),
+                          ),
+                          Text(
+                            'XX',
+                            style: TextStyle(
+                                fontFamily: 'Raleway',
+                                color: RequisitaCor.requisitaAzul(30),
+                                fontSize: 20),
+                          ),
+                        ],
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          topLeft: Radius.circular(20),
+                        ),
+                        child: Container(
+                            color: RequisitaCor.requisitaAzul(30),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 3, horizontal: 6),
+                            child: Text(
+                              'Coletanea',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
+                            )),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 10,
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: 2,
+      ),
     );
   }
 }
