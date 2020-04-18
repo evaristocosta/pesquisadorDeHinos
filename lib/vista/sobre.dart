3f26ff8165f4a26babb64679805c9d5cc65cb1bf
Lucas Costa <lucascosta74@gmail.com>
2020-04-18 19:52:42 -0300

    inicio da estrutura da tela

---
+++ lib/vista/sobre.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Sobre extends StatefulWidget {
+  @override
+  _SobreState createState() => _SobreState();
+}
+
+class _SobreState extends State<Sobre> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: Text('Sobre',
+            style: TextStyle(color: Colors.white, fontFamily: 'Raleway')),
+      ),
+      body: Container(),
+    );
+  }
+}
