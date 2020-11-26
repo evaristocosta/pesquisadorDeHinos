d0a7552bdc68a0f32ea63537022afa17fa14d308
Lucas Costa <lucascosta74@gmail.com>
2020-11-26 03:10:50 -0300

    componentizacao de carregamento

---
+++ lib/view/components/carregando.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Carregando extends StatelessWidget {
+  const Carregando({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: SizedBox(
+        child: CircularProgressIndicator(),
+        height: 75.0,
+        width: 75.0,
+      ),
+    ));
+  }
+}
