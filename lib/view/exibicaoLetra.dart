0e274fe53fa0fac790f63c3afa934cfbaeb99eb9
Lucas Costa <lucascosta74@gmail.com>
2020-12-13 23:32:38 -0300

    tamanho de letra alteravel

--- lib/view/exibicaoLetra.dart
+++ lib/view/exibicaoLetra.dart
@@ -37,82 +37,126 @@ class _ExibicaoLetraState extends State<ExibicaoLetra> {
     DERRAMA DO TEU SANGUE SOBRE MIM.
   """;
 
+  double _tamanhoTexto = 14.0;
+
+  void _aumentaTexto() {
+    setState(() {
+      if (_tamanhoTexto <= 30.0) _tamanhoTexto += 1.0;
+    });
+  }
+
+  void _diminuiTexto() {
+    setState(() {
+      if (_tamanhoTexto >= 10.0) _tamanhoTexto -= 1.0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: () => _diminuiTexto(),
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.remove,
+              color: RequisitaEstilo.azul(30),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          FloatingActionButton(
+            heroTag: null,
+            onPressed: () => _aumentaTexto(),
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.add,
+              color: RequisitaEstilo.azul(30),
+            ),
+          ),
+        ],
+      ),
       resizeToAvoidBottomInset: false,
       body: SafeArea(
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: <Widget>[
-                        Text(
-                          indicador + ' ',
-                          style: TextStyle(
-                              fontFamily: 'Raleway',
-                              color: RequisitaEstilo.azul(20)),
-                        ),
-                        Text(
-                          numero,
-                          style: TextStyle(
-                              fontFamily: 'Raleway',
-                              color: RequisitaEstilo.azul(30),
-                              fontSize: 24),
-                        ),
-                      ],
-                    ),
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(20),
-                        topLeft: Radius.circular(20),
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            indicador + ' ',
+                            style: TextStyle(
+                                fontFamily: 'Raleway',
+                                color: RequisitaEstilo.azul(20)),
+                          ),
+                          Text(
+                            numero,
+                            style: TextStyle(
+                                fontFamily: 'Raleway',
+                                color: RequisitaEstilo.azul(30),
+                                fontSize: 24),
+                          ),
+                        ],
                       ),
-                      child: Container(
-                          color: RequisitaEstilo.azul(30),
-                          padding:
-                              EdgeInsets.symmetric(vertical: 3, horizontal: 6),
-                          child: Text(
-                            coletanea,
-                            style: TextStyle(color: Colors.white, fontSize: 10),
-                          )),
-                    )
-                  ],
+                      ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          topLeft: Radius.circular(20),
+                        ),
+                        child: Container(
+                            color: RequisitaEstilo.azul(30),
+                            padding: EdgeInsets.symmetric(
+                                vertical: 3, horizontal: 6),
+                            child: Text(
+                              coletanea,
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 10),
+                            )),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Text(
-                categoria,
-                style: TextStyle(
-                    color: RequisitaEstilo.cinza(30),
+                Text(
+                  categoria,
+                  style: TextStyle(
+                      color: RequisitaEstilo.cinza(30),
+                      fontFamily: 'Raleway',
+                      fontSize: 12),
+                ),
+                Text(
+                  nome,
+                  style: TextStyle(
+                    fontSize: 20,
                     fontFamily: 'Raleway',
-                    fontSize: 12),
-              ),
-              Text(
-                nome,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontFamily: 'Raleway',
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 22.0),
-                child: Divider(
-                  color: RequisitaEstilo.cinza(40),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 22.0),
+                  child: Divider(
+                    color: RequisitaEstilo.cinza(40),
+                  ),
                 ),
-              ),
-              Html(
-                data: texto,
-              ),
-            ],
+                Html(
+                  data: texto,
+                  defaultTextStyle: TextStyle(fontSize: _tamanhoTexto),
+                ),
+              ],
+            ),
           ),
         ),
       ),
