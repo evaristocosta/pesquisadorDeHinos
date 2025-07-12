876728f2001b7f10478f7568c3e37166282241f0
Lucas Costa <lucascosta74@gmail.com>
2025-07-12 14:11:46 +0200

    configuracoes do firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,13 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-messaging")
+}
+
 android {
     namespace = "com.trabalhosgerais.pesquisadorhinos"
     compileSdk = flutter.compileSdkVersion
